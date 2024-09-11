78a857bedddd5e08aa8731fe3b6b2f8434b3ead2
MindfulLearner <dimajosh1009@gmail.com>
2024-09-11 00:36:21 +0000

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<script>
+    export default {
+        data() {
+            return {
+                messaggioComponente: "swag",
+            }
+        }
+    }
+</script>
+
+<template>
+    <div>
+        <h1>
+            {{ messaggioComponente }}
+        </h1>
+    </div>
+</template>
+
+<style scoped>
+    h1 {
+        color: white;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
+import './globale.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
